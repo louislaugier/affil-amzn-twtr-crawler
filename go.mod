84567963c8edd656b109bcf1cf683315b1e21b88
louislaugier <l.laugier@protonmail.com>
2021-12-26 21:37:06 +0100

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/louislaugier/affil-amzn-twtr-crawler
 
 go 1.15
 
-require github.com/gocolly/colly/v2 v2.1.0
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/joho/godotenv v1.4.0
+	github.com/mmcdole/gofeed v1.1.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,35 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 
 	colly "github.com/gocolly/colly/v2"
+	"github.com/joho/godotenv"
 )
 
-func checkAccountLogged(c *colly.Collector) bool {
-	logged := false
-	c.OnHTML("a[data-nav-ref]", func(e *colly.HTMLElement) {
-		if !strings.Contains(e.Text, "Sign in") {
-			// browser logged in
-			logged = true
-		}
+type rawDealList struct {
+	PrefetchedData struct {
+		AapiGetDealsList []struct {
+			Entities []struct {
+				Resource struct {
+					URL string `json:"url"`
+				} `json:"resource"`
+				Entity struct {
+					Details struct {
+						Entity struct {
+							Price struct {
+								Details struct {
+									DealPrice struct {
+										MoneyValueOrRange struct {
+											Range struct {
+												Min struct {
+													Amount string `json:"amount"`
+												} `json:"min"`
+												Max struct {
+													Amount string `json:"amount"`
+												} `json:"max"`
+											} `json:"range"`
+										} `json:"moneyValueOrRange"`
+									} `json:"dealPrice"`
+								} `json:"details"`
+							} `json:"price"`
+							EndTime struct {
+								Value time.Time `json:"value"`
+							} `json:"endTime"`
+							Type  string `json:"type"`
+							Title string `json:"title"`
+						} `json:"entity"`
+					} `json:"details"`
+				} `json:"entity"`
+			} `json:"entities"`
+		} `json:"aapiGetDealsList"`
+	} `json:"prefetchedData"`
+}
+
+type deal struct {
+	Title   string
+	Price   float64
+	URL     string
+	Type    string
+	EndDate time.Time
+}
+
+// &tag=
+func getDeals(c *colly.Collector, page int) []deal {
+	deals := []deal{}
+	c.OnHTML("html", func(e *colly.HTMLElement) {
+		// format products json string
+		str := strings.Replace(strings.Replace(strings.Split(strings.Split(strings.Split(e.Text, "window.P.when('DealsWidgetsHorizonteAssets').execute(function (assets) {")[1], "});")[0], "assets.mountWidget('slot-15', ")[1], "\n", "", -1), ")            ", "", -1)
+
+		// json string to map
+		rawDeals := rawDealList{}
+		json.Unmarshal([]byte(str), &rawDeals)
+
+		fmt.Println(rawDeals.PrefetchedData.AapiGetDealsList[0].Entities[0])
 	})
-	c.Visit("https://amazon.com/deals")
-	return logged
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://www.amazon.com/gp/goldbox?ref_=nav_cs_gb&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A" + strconv.Itoa(60*page-60) + "%252C%2522presetId%2522%253A%2522024BF8E73BAAA8C1AB5B6D205172D8CE%2522%252C%2522sorting%2522%253A%2522BY_CUSTOM_CRITERION%2522%257D")
+	return deals
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
 	c := colly.NewCollector()
-	isAccountLogged := checkAccountLogged(c)
-	fmt.Println(isAccountLogged)
+	getDeals(c, 1)
+	// dealsPage1 := getDeals(c, 1)
+	// dealsPage2 := getDeals(c, 2)
+	// fmt.Println(dealsPage1, dealsPage2)
 }
-
-// Find and visit all links
-// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-// 	e.Request.Visit(e.Attr("href"))
-// })
-// On link click
-// c.OnRequest(func(r *colly.Request) {
-// 	fmt.Println("Visiting", r.URL)
-// })
