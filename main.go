e183dd93995336ae9a177b79bebfc636f3ad57bf
louislaugier <l.laugier@protonmail.com>
2022-01-09 20:46:20 +0000

    update

--- main.go
+++ main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/louislaugier/affil-amzn-twtr-crawler/deal"
 	"github.com/louislaugier/affil-amzn-twtr-crawler/follower"
 )
 
-func schedule() {
-	mins := time.Now().Minute()
-	if mins%3 == 0 {
-		deal.GetDeals()
-	}
-	if mins%15 == 0 || mins == 0 {
-		follower.GetAmazonFollowerList()
-	}
-	time.Sleep(52 * time.Second)
-	schedule()
-}
+// func schedule() {
+// 	mins := time.Now().Minute()
+// 	if mins%3 == 0 {
+// 		deal.GetDeals()
+// 	}
+// 	if mins%15 == 0 || mins == 0 {
+// 		follower.GetAmazonFollowerList()
+// 	}
+// 	time.Sleep(52 * time.Second)
+// 	schedule()
+// }
 
 func main() {
 	// if no Amazon affiliate tag env variable found, load .env file
