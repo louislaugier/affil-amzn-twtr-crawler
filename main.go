c5682fad43edc24a1c21d627ff0cbddbdff853d5
louislaugier <l.laugier@protonmail.com>
2022-01-09 19:25:55 +0000

    update

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func schedule() {
 	if mins%15 == 0 || mins == 0 {
 		follower.GetAmazonFollowerList()
 	}
-	time.Sleep(52 * time.Second)
-	schedule()
+	// time.Sleep(52 * time.Second)
+	// schedule()
 }
 
 func main() {
